4b592ba360bd0a4b36ed93a7204e68b30d8e7d66
Curt J. Sampson <cjs@cynic.net>
2021-09-07 01:08:15 +0900

    Import CSCP version 2021-02-07
    From: http://takeda-toshiya.my.coocan.jp/common/history/2021-02-07.7z

--- src/vm/bx1/printer.h
+++ src/vm/bx1/printer.h
@@ -2,42 +2,52 @@
 	CANON BX-1 Emulator 'eBX-1'
 
 	Author : Takeda.Toshiya
-	Date   : 2020.08.22-
+	Date   : 2021.02.06-
 
-	[ keyboard ]
+	[ printer ]
 */
 
-#ifndef _KEYBOARD_H_
-#define _KEYBOARD_H_
+#ifndef _PRINTER_H_
+#define _PRINTER_H_
 
 #include "../vm.h"
 #include "../../emu.h"
 #include "../device.h"
 
-class FIFO;
+class FILEIO;
 
-class KEYBOARD : public DEVICE
+class PRINTER : public DEVICE
 {
 private:
-	FIFO *fifo_down;
-	FIFO *fifo_up;
+	uint8_t *ram;
+	FILEIO *fio;
+	uint8_t column, htab;
+	uint8_t e210, e211;
+	
+	void output(uint8_t);
 	
 public:
-	KEYBOARD(VM_TEMPLATE* parent_vm, EMU* parent_emu) : DEVICE(parent_vm, parent_emu)
+	PRINTER(VM_TEMPLATE* parent_vm, EMU* parent_emu) : DEVICE(parent_vm, parent_emu)
 	{
-		set_device_name(_T("Keyboard"));
+		set_device_name(_T("Printer"));
 	}
-	~KEYBOARD() {}
+	~PRINTER() {}
 	
 	// common functions
 	void initialize();
 	void release();
 	void reset();
+	void event_frame();
+	void event_vline(int v, int clock);
 	void write_io8(uint32_t addr, uint32_t data);
 	uint32_t read_io8(uint32_t addr);
 	bool process_state(FILEIO* state_fio, bool loading);
 	
 	// unique functions
+	void set_context_ram(uint8_t* ptr)
+	{
+		ram = ptr;
+	}
 	void key_down(int code);
 	void key_up(int code);
 };
